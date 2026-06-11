08b124179fdb8d0204a3ba4b95f2bf00c937c1b9
Megha <annmeghaanil@gmail.com>
2026-06-11 11:53:24 +0530

    Added root route

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+    res.json({
+        message: 'User Authentication API is running'
+    })
+})
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/notes', require('./routes/noteRoutes'));
 
